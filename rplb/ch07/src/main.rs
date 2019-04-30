f7e915e0fda58cb6c096f7223dd84c8b4e9e523d
Nathan Givens <nate.givens03@gmail.com>
2019-04-30 17:07:53 -0400

    added notes but didn't get very far in chapter

--- rplb/ch07/src/main.rs
+++ rplb/ch07/src/main.rs
@@ -1,3 +1,12 @@
+/*
+ * Packages Crates and Modules
+ *
+ * - Packages: Cargo feature that allows to build, test, and share crates
+ * - Crates: tree of modules that produce a library of executable
+ * - Modules: 'use' keyword let you control the scope and privacy of paths
+ * - A path: way of naming an item such as a struct, function, or module
+ */
+
 fn main() {
     println!("Hello, world!");
 }
