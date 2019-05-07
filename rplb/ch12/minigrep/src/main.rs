03c1e4876910c4fc0f206a4446c74934a92e9dba
Nathan Givens <nate.givens03@gmail.com>
2019-05-07 00:15:29 -0400

    finished ch 12: i/o - building mini grep

--- rplb/ch12/minigrep/src/main.rs
+++ rplb/ch12/minigrep/src/main.rs
@@ -1,4 +1,7 @@
 use std::env;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     // std::env::args will panic! on any invalid unicode
@@ -7,5 +10,16 @@ fn main() {
     // .collect() requires that the type be specified because it can format the
     // return value in many different ways
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    // let query = &args[1];
+    // let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
 }
