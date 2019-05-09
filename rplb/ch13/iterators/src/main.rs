eb03ecd659fccb1aca89a2428776f66622c9cbe4
Nathan Givens <nate.givens03@gmail.com>
2019-05-08 23:27:58 -0400

    finished ch 13: Iterators and Closures

---
+++ rplb/ch13/iterators/src/main.rs
@@ -0,0 +1,73 @@
+/*
+ *  filters by shoe size
+ */
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+/*
+ *  creating my own iterator
+ */
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+fn main() {
+    /*
+     *  filters by shoe size
+     */
+    let shoes = vec![
+        Shoe {
+            size: 10,
+            style: String::from("sneaker"),
+        },
+        Shoe {
+            size: 13,
+            style: String::from("sandal"),
+        },
+        Shoe {
+            size: 10,
+            style: String::from("boot"),
+        },
+    ];
+
+    println!("shoes: {:#?}", shoes);
+    let in_my_size = shoes_in_my_size(shoes, 10);
+    println!("shoes in my size: {:#?}", in_my_size);
+
+    /*
+     *  creating my own iterator
+     */
+    let my_count = Counter::new();
+    let sum: u32 = my_count
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| a * b)
+        .filter(|x| x % 3 == 0)
+        .sum();
+    println!("and our sum is... {}", sum);
+}
