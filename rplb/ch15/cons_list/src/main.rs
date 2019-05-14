84916b32c9b61f50595000a87833514d51b33dad
Nathan Givens <nate.givens03@gmail.com>
2019-05-14 15:51:23 -0400

    finished ch 15.5: refcell<T> and the interior mutability pattern

--- rplb/ch15/cons_list/src/main.rs
+++ rplb/ch15/cons_list/src/main.rs
@@ -1,25 +1,23 @@
 #[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
 use crate::List::{Cons, Nil};
 use std::rc::Rc;
+use std::cell::RefCell;
 
 fn main() {
     // let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-        println!("{:?}", c);
-    }
+    let value = Rc::new(RefCell::new(5));
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
 
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    *value.borrow_mut() += 10;
+
     println!("{:?}", a);
     println!("{:?}", b);
+    println!("{:?}", c);
 }
