997c152252549d9023d53f108da04e919796106a
Nathan Givens <nate.givens03@gmail.com>
2019-05-22 09:09:18 -0400

    using Vec in main to dynamically add elements in the future

--- rsorts/src/main.rs
+++ rsorts/src/main.rs
@@ -1,9 +1,9 @@
 use rsorts::rsorts::qsort;
 
 fn main() {
-    let mut a1 = [5, 3, 6, 4, 7, 2, 1, 10, 9];
+    let mut a1 = vec![5, 3, 6, 4, 7, 2, 1, 10, 9];
     // println!("{:?}", a1);
-    qsort(&mut a1, |a, b| a - b);
+    qsort(a1.as_mut_slice(), |a, b| a - b);
     println!("{:?}", a1);
     // test_slice(&a1[1..]);
 }
