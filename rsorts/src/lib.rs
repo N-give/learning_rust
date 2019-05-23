d38e4e588fd59c0fc8fc2f6ed7a988f27aeab393
Nathan Givens <nate.givens03@gmail.com>
2019-05-23 13:12:08 -0400

    qsort works for generics with Eq, Ord, and Copy traits

--- rsorts/src/lib.rs
+++ rsorts/src/lib.rs
@@ -1,34 +1,29 @@
 pub mod rsorts {
-    use std::thread;
-
-    pub fn rqsort<'a>(arr: &'a mut [i32], rev: bool) {
+    pub fn gen_rqsort<T> (arr: &mut[T], rev: bool)
+        where T: Eq + Ord + Copy,
+    {
         if arr.len() <= 1 {
             return;
         }
         let mut front: usize;
         let mut back: usize;
         let cmp = arr[arr.len() - 1];
-        println!("begin:\n{:?}", arr);
-        println!("cmp: {}", cmp);
 
         while {
             front = match arr[..(arr.len())]
                 .iter()
                 .position(|n| {
-                    println!("{}", n);
                     n > &cmp
                 })
             {
                 Some(i) => i,
                 None => arr.len() - 1,
             };
-            println!("front: {}, value: {}", front, arr[front]);
 
             back = match arr[..(arr.len() - 1)]
                     .iter()
                     .rev()
                     .position(|n| {
-                        println!("{}", n);
                         n <= &cmp
                     })
             {
@@ -36,25 +31,13 @@ pub mod rsorts {
                 None => 0,
             };
 
-            println!("back: {}, value: {}", back, arr[back]);
             front < back
         } {
-            let tmp = arr[front];
-            arr[front] = arr[back];
-            arr[back] = tmp;
-            println!("intermediate: {:?}", arr);
+            // std::mem::swap(&mut arr[front], &mut arr[back]);
+            arr.swap(front, back);
         }
-        let tmp = arr[front];
-        arr[front] = arr[arr.len() - 1];
-        arr[arr.len() - 1] = tmp;
-        // println!("end:\n{:?}", arr);
-
-        let t1 = thread::spawn(|| {rqsort(&mut arr[..front], rev)});
-        let t2 = thread::spawn(|| {rqsort(&mut arr[(front+1)..], rev)});
-
-        t1.join().unwrap();
-        t2.join().unwrap();
-        // rqsort(&mut arr[..front], false);
-        // rqsort(&mut arr[(front+1)..], false);
+        arr.swap(front, arr.len() - 1);
+        gen_rqsort(&mut arr[..front], rev);
+        gen_rqsort(&mut arr[(front + 1)..], rev);
     }
 }
--- rsorts/src/main.rs
+++ rsorts/src/main.rs
@@ -1,11 +1,16 @@
-use rsorts::rsorts::rqsort;
+use rsorts::rsorts::{rqsort, gen_rqsort};
+extern crate rand;
+use rand::Rng;
+// use std::io::BufReader;
 
 fn main() {
     // let mut a1 = vec![1, 3, 2];
-    let mut a1 = vec![1, 8, 6, 4, 7, 2, 9, 10, 5];
+    // let mut a1 = vec![1, 8, 6, 4, 7, 2, 9, 10, 5, 12, 60, 34, 76, 8, 5, 23, 5, 4,33, 2, 6, 56, 7, 8, 54, 34, 65, 7, 8, 56, 3, 4, 5, 7, 5, 6, 345];
     //  test_pos(&a1);
     // println!("{:?}", a1);
-    rqsort(a1.as_mut_slice(), false);
-    println!("{:?}", a1);
+    let mut nums = [0i8; 32];
+    rand::thread_rng().fill(&mut nums);
+    gen_rqsort(&mut nums, false);
+    println!("{:#?}", nums);
     // test_slice(&a1[1..]);
 }
