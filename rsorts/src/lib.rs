9bdc31ef07d568fcbeca5831a85952aa65696c8d
Nathan Givens <nate.givens03@gmail.com>
2019-05-23 10:36:29 -0400

    starting to separate into threads

--- rsorts/src/lib.rs
+++ rsorts/src/lib.rs
@@ -1,34 +1,41 @@
 pub mod rsorts {
-    pub fn rqsort(arr: &mut [i32], rev: bool) {
-        if arr.len() == 1 {
+    use std::thread;
+
+    pub fn rqsort<'a>(arr: &'a mut [i32], rev: bool) {
+        if arr.len() <= 1 {
             return;
         }
-        let mut front = 0usize;
-        let mut back = (arr.len() - 2) as usize;
+        let mut front: usize;
+        let mut back: usize;
         let cmp = arr[arr.len() - 1];
         println!("begin:\n{:?}", arr);
         println!("cmp: {}", cmp);
 
         while {
-            front = arr[..(arr.len())]
+            front = match arr[..(arr.len())]
                 .iter()
                 .position(|n| {
                     println!("{}", n);
                     n > &cmp
                 })
-                .unwrap();
+            {
+                Some(i) => i,
+                None => arr.len() - 1,
+            };
             println!("front: {}, value: {}", front, arr[front]);
 
-            back = arr.len()
-                - 2
-                - arr[..(arr.len() - 1)]
+            back = match arr[..(arr.len() - 1)]
                     .iter()
                     .rev()
                     .position(|n| {
                         println!("{}", n);
                         n <= &cmp
                     })
-                    .unwrap();
+            {
+                Some(i) => arr.len() - 2 - i,
+                None => 0,
+            };
+
             println!("back: {}, value: {}", back, arr[back]);
             front < back
         } {
@@ -41,5 +48,13 @@ pub mod rsorts {
         arr[front] = arr[arr.len() - 1];
         arr[arr.len() - 1] = tmp;
         // println!("end:\n{:?}", arr);
+
+        let t1 = thread::spawn(|| {rqsort(&mut arr[..front], rev)});
+        let t2 = thread::spawn(|| {rqsort(&mut arr[(front+1)..], rev)});
+
+        t1.join().unwrap();
+        t2.join().unwrap();
+        // rqsort(&mut arr[..front], false);
+        // rqsort(&mut arr[(front+1)..], false);
     }
 }
--- rsorts/src/main.rs
+++ rsorts/src/main.rs
@@ -1,6 +1,7 @@
 use rsorts::rsorts::rqsort;
 
 fn main() {
+    // let mut a1 = vec![1, 3, 2];
     let mut a1 = vec![1, 8, 6, 4, 7, 2, 9, 10, 5];
     //  test_pos(&a1);
     // println!("{:?}", a1);
