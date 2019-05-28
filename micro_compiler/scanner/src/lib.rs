aa38be505e8eb147e4198c22cb4f422ae5dc3101
nate <nate.givens03@gmail.com>
2019-05-27 22:20:03 -0400

    still testing regex matching

--- micro_compiler/scanner/src/lib.rs
+++ micro_compiler/scanner/src/lib.rs
@@ -7,22 +7,18 @@ use std::io::prelude::*;
 use std::io::BufReader;
 
 static REGEX_IDENTIFIER: &str = r"\w[a-zA-Z0-9]+";
-static REGEX_INTLITERAl: &str = r"\d+";
+static REGEX_INTLITERAL: &str = r"\d+";
 static REGEX_FLOATLITER: &str = r"\d*\.\d+";
 static REGEX_STRINGLITERAL: &str = r#""\.*""#;
 static REGEX_COMMENT: &str = r"--\.*$";
-static REGEX_OPERATORS: &str = r":=|+|-|*|/|=|!=|<|>|(|)|;|,|<=|>=|";
-
-static MICRO_KEYWORDS: Vec<str> = vec![
-    "PROGRAM", "BEGIN", "END", "FUNCTION", "READ", "WRITE", "IF", "ELSE", "ENDIF", "WHILE",
-    "ENDWHILE", "RETURN", "INT", "VOID", "STRING", "FLOAT", "TRUE", "FALSE", "FOR", "ENDFOR",
-    "CONTINUE", "BREAK",
-];
+static REGEX_OPERATORS: &str = r":=|\+|-|\*|/|=|!=|<|>|(|)|;|,|<=|>=";
+static REGEX_KEYWORDS: &str = r"(?x) PROGRAM|BEGIN|END|FUNCTION|READ|WRITE|IF|ELSE|ENDIF|WHILE|ENDWHILE|RETURN|INT|VOID|STRING|FLOAT|TRUE|FALSE|FOR|ENDFOR|CONTINUE|BREAK";
 
 pub fn scan_file(fp: std::fs::File) -> Result<HashMap<String, String>, std::io::Error> {
     let tokens: HashMap<String, String> = HashMap::new();
     let reader = BufReader::new(fp);
-    let tre = Regex::new(REGEX_IDENTIFIER).unwrap();
+    // TODO: need to test for multiple matches on a line
+    let tre = Regex::new(REGEX_OPERATORS).unwrap();
 
     for line in reader.lines() {
         let line = line.expect("failed to read line");
